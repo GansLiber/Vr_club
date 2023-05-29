8763252dbcaf06d2568e231a532c58208684a530
zazaz <zazaza993@mail.ru>
2023-05-29 14:36:16 +0700

    try to do slider, install side component, and can't out the img, idk why, will be figure out

---
+++ src/components/homePage/ThirdScreen.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class='main-third'>
+    <img src='@/assets/home/thirdScreen/slide1.png' alt='img' />
+    <Swiper>
+      <SwiperSlide v-for='photo in photos'>
+        <img :src='`@/assets/home/thirdScreen/${photo}`' alt='img'>
+      </SwiperSlide>
+    </Swiper>
+  </div>
+</template>
+
+<script>
+import {Swiper, SwiperSlide} from 'swiper/vue'
+import 'swiper/swiper-bundle.css'
+
+export default {
+  name: 'kursThirdScreen',
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  data() {
+    return {
+      photos: [
+        'slide1.png',
+        'slide2.png',
+        'slide3.png'
+      ]
+    }
+  }
+  // Другие опции компонента здесь
+}
+</script>
+
+<style scoped>
+.main-third {
+  height: 20em;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,21 @@
   <div class='main'>
     <kurs-first-screen />
     <kurs-twice-screen />
+    <kurs-third-screen />
   </div>
 </template>
 
 <script>
 import kursFirstScreen from '@/components/homePage/FirstScreen'
 import kursTwiceScreen from '@/components/homePage/TwiceScreen'
+import kursThirdScreen from '@/components/homePage/ThirdScreen'
 
 export default {
   name: 'Home',
   components: {
     kursFirstScreen,
-    kursTwiceScreen
+    kursTwiceScreen,
+    kursThirdScreen
   },
 
 
