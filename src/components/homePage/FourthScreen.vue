a9f673e42d24491ca9ceb371d495dd78a9224d9a
zazaz <zazaza993@mail.ru>
2023-05-30 14:41:07 +0700

    finish video

--- src/components/homePage/FourthScreen.vue
+++ src/components/homePage/FourthScreen.vue
@@ -1,6 +1,7 @@
 <template>
   <div class='fourth_home_screen'>
-    <p>Видео</p>
+    <p class='advantage-text'>Видеопредставление нашего раскумара</p>
+
     <vue-plyr :options='options'>
       <video
         controls
@@ -18,28 +19,34 @@
           src='@/assets/home/video/gg.mp4'
           type='video/mp4'
         />
-
       </video>
     </vue-plyr>
-
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'kursFourthScreen',
   data() {
     return {
       options: {
-        poster: '@/assets/home/video/poster.jpg'
+        poster: require('@/assets/home/video/poster.png')
       }
     }
   }
-
 }
 </script>
 
 <style scoped>
 
+.advantage-text {
+  font-style: normal;
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 113.19%;
+  text-align: center;
+  letter-spacing: 0.05em;
+  color: #FFFFFF;
+  margin: 2em;
+}
 </style>
